7ece32e3d0d0d0a1989591187a195fc3efe1efd2
scitechkz <tdprof@gmail.com>
2025-11-09 20:55:09 +0100

    seperate the start and build command. build is now creating secret/

--- render-start.sh
+++ render-start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-#This file needs to be made executable to run: chmod +x render-start.sh
-# 1. Ensure the secrets directory exists
-mkdir -p /etc/secrets
 
-# 2. Decode the Base64 credential and save it as a JSON file
-echo "$GOOGLE_CREDENTIALS_BASE64" | base64 -d > /etc/secrets/google_credentials.json
-
-# 3. Start the Gunicorn/Uvicorn server
+# Start the Gunicorn/Uvicorn server
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornH11Worker --bind 0.0.0.0:$PORT
